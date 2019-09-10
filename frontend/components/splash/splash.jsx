62cc69829acad432cec784b41137c98d0d417f87
Annie Giang <annieg4123@gmail.com>
2019-09-10 15:27:21 -0700

    added splash background gradient

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 class Splash extends React.Component {
     render() {
         return (
-            <div>
+            <div className="homepage-video">
                 {/* <img className="homepage-video" src="assets/splash-temp.jpg"/> */}
                 <video 
                     autoPlay loop
                     src="assets/homepagevideo_short_version2.98c5bfce.mp4"
-                    className="homepage-video"
                     type='video/mp4'
                 >
-                    
                 </video>
+                <div class="homepage-background">
 
+                </div>
             </div>
         )
     }
