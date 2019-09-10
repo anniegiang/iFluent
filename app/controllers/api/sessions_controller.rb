f199fc6d7dfe3cbb79b7598757061a039feaf5db
Annie Giang <annieg4123@gmail.com>
2019-09-09 22:00:07 -0700

    finished sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,27 @@
 class Api::SessionsController < ApplicationController
+
+	def create
+		@user = User.find_by_creds(params[:user][:email], params[:user][:password])
+
+		if @user
+			login!(@user)
+			render :show
+		else
+			if !User.exists?(:email => params[:user][:email])
+				render json: ["User does not exist"], status: 422
+			else
+				render json: ["Invalid credentials"], status: 422
+			end
+		end
+	end
+
+	def destroy
+		@user = current_user
+		if @user
+			logout!
+			render json: {}
+		else
+			render json: ["No one signed in"], status: 422
+		end
+	end
 end
