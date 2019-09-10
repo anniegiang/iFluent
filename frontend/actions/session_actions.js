67b46beb7d8961492f472105781b70fe25730da4
Annie Giang <annieg4123@gmail.com>
2019-09-09 22:28:49 -0700

    created auth actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,22 +1,40 @@
-export const signup = user => {
-  return $.ajax({
-    url: '/api/users',
-    method: 'POST',
-    data: { user }
-  })
-}
+import { login, logout, signup } from '../util/session_api_util';
 
-export const login = user => {
-  return $.ajax({
-    url: '/api/session',
-    method: 'POST',
-    data: { user }
-  })
-}
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
 
-export const logout = () => {
-  return $.ajax({
-    url: '/api/session',
-    method: 'DELETE'
-  })
-}
+const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+const receiveErrors = errors => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+});
+
+export const clearErrors = () => ({
+  type: CLEAR_ERRORS
+});
+
+// Thunk action creators
+
+export const signupUser = user => dispatch => signup(user)
+  .then(user => dispatch(receiveCurrentUser(user)),
+    errors => dispatch(receiveErrors(errors.responseJSON))
+  )
+
+export const loginUser = user => dispatch => login(user)
+  .then(user => dispatch(receiveCurrentUser(user)),
+    errors => dispatch(receiveErrors(errors.responseJSON))
+  );
+
+export const logoutUser = () => dispatch => logout()
+  .then(() => dispatch(logoutCurrentUser()));
+
