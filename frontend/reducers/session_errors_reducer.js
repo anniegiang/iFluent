50ae0dcdbe6219c4f1b14e9397f01aeff0400e17
Annie Giang <annieg4123@gmail.com>
2019-09-09 22:31:46 -0700

    created session errors reducer

---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER, CLEAR_ERRORS } from '../actions/session_actions';
+
+const _nullErrors = [];
+
+export const sessionErrorsReducer = (state = _nullErrors, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors
+    case RECEIVE_CURRENT_USER:
+      return _nullErrors;
+    case CLEAR_ERRORS:
+      return _nullErrors;
+    default:
+      return state;
+  }
+}
+
+export default sessionErrorsReducer;
+
