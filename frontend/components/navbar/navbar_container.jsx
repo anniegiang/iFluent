971b12c0d96bf2cfe8d1aa98fe4505004774b14e
Annie Giang <annieg4123@gmail.com>
2019-09-09 22:48:15 -0700

    created navbar component

---
+++ frontend/components/navbar/navbar_container.jsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import NavBar from './navbar';
+import { logoutUser, loginUser } from '../../actions/session_action';
+import { withRouter } from 'react-router-dom'
+
+const mapStateToProps = state => {
+  return {
+    currentUser: state.entities.users[state.session.id]
+  }
+};
+
+const mapDispatchToProps = dispatch => ({
+  login: user => dispatch(loginUser(user)),
+  logout: () => dispatch(logoutUser())
+});
+
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(NavBar));
