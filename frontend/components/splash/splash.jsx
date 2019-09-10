eae3be76a2c88885b626eb84a3610fb4c578c730
Annie Giang <annieg4123@gmail.com>
2019-09-10 14:42:37 -0700

    video loops

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -6,9 +6,9 @@ class Splash extends React.Component {
             <div>
                 {/* <img className="homepage-video" src="assets/splash-temp.jpg"/> */}
                 <video 
-                    loop="" 
+                    loop="true" 
                     playsinline="" 
-                    autoplay="" 
+                    autoplay="true" 
                     poster="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/hero-img.5353d1d3.jpg" 
                     src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/homepagevideo_short_version2.98c5bfce.mp4"
                     className="homepage-video"
