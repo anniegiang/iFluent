b6a2fbe1d106804c0cd7938802339407078b1071
Annie Giang <annieg4123@gmail.com>
2019-09-09 22:40:38 -0700

    added simple root and app components

---
+++ frontend/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+const App = () => (
+  <div>
+    <header>
+      <h1>iFluent</h1>
+    </header>
+  </div>
+);
+
+export default App;
