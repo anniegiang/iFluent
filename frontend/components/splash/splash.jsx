ffb93a89f2475747a43484300f6bdfe1eda4c44e
Annie Giang <annieg4123@gmail.com>
2019-09-10 14:34:47 -0700

    replaced splash with video

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,7 +4,18 @@ class Splash extends React.Component {
     render() {
         return (
             <div>
-                <img className="homepage-pic" src="assets/splash-temp.jpg"/>
+                {/* <img className="homepage-video" src="assets/splash-temp.jpg"/> */}
+                <video 
+                    loop="" 
+                    playsinline="" 
+                    autoplay="" 
+                    poster="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/hero-img.5353d1d3.jpg" 
+                    src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/homepagevideo_short_version2.98c5bfce.mp4"
+                    className="homepage-video"
+                >
+                    
+                </video>
+
             </div>
         )
     }
