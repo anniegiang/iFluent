225f422283fddd69ce55d600f6f1a46adc2faa87
Annie Giang <annieg4123@gmail.com>
2019-09-10 15:09:09 -0700

    replaced video src with local mp4

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -6,12 +6,10 @@ class Splash extends React.Component {
             <div>
                 {/* <img className="homepage-video" src="assets/splash-temp.jpg"/> */}
                 <video 
-                    loop="true" 
-                    playsinline="" 
-                    autoplay="true" 
-                    poster="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/hero-img.5353d1d3.jpg" 
-                    src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/homepagevideo_short_version2.98c5bfce.mp4"
+                    autoPlay loop
+                    src="assets/homepagevideo_short_version2.98c5bfce.mp4"
                     className="homepage-video"
+                    type='video/mp4'
                 >
                     
                 </video>
