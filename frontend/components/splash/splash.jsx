b641a1f2baf621411fd219553964c75362a69530
Annie Giang <annieg4123@gmail.com>
2019-09-10 11:53:15 -0700

    added temp splash pic

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,7 +4,7 @@ class Splash extends React.Component {
     render() {
         return (
             <div>
-                <h1>splash gif here</h1>
+                <img src="assets/splash-temp.jpg"/>
             </div>
         )
     }
