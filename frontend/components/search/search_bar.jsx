fc278f0b3c1f2546407091d2ba41e2b44927c4cc
Annie Giang <annieg4123@gmail.com>
2019-09-10 14:22:58 -0700

    positioned search bar

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -6,10 +6,12 @@ class SearchBar extends React.Component {
             <div className="search-container">
                 <h1><span>BECOME FLUENT IN ANY LANGUAGE</span></h1>
                 <p><span>Choose from over 10,000 teachers for 1-on-1 lessons based on your goals and interests.</span></p>
-                <input className="searchbar" type="text"  placeholder="Choose a language"/>
-                <button className="search-btn">
-                    <img src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/icon-search.5d60d25a.svg"/>
-                </button>
+                <div className="searchbar">
+                    <input className="search-input" type="text"  placeholder="Choose a language"/>
+                    <button className="search-btn">
+                        <img src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/icon-search.5d60d25a.svg"/>
+                    </button>
+                </div>
             </div>
         )
     }
