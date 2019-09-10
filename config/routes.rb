9f7180838fecb6ea76917ab696b9668c3e593fb5
Annie Giang <annieg4123@gmail.com>
2019-09-09 22:12:48 -0700

    added root html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	namespace :api, default: {format: :json} do
-		resources :users, only: [:create]
+		resources :users, only: [:index, :create]
 	    resource :session, only: [:create, :destroy]
 	end
 	root to: 'static_pages#root'
