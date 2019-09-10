a09e13757720884beda4ecc828af1aa21ae21321
Annie Giang <annieg4123@gmail.com>
2019-09-10 13:36:53 -0700

    added text and basic search input

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -4,7 +4,10 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div>
-                Search Bar <input type="text" />
+                <h1><span>BECOME FLUENT IN ANY LANGUAGE</span></h1>
+                <p><span>Choose from over 10,000 teachers for 1-on-1 lessons based on your goals and interests.</span></p>
+                Search Bar <input type="text"  placeholder="Choose a language"/>
+                <button>Search</button>
             </div>
         )
     }
