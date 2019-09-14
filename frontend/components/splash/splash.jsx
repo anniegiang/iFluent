6fa797d794f0c8f012c095fdbeb07bee6e5ed02b
Annie Giang <annieg4123@gmail.com>
2019-09-13 21:54:39 -0700

    added gradient to main nac

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,10 +4,10 @@ class Splash extends React.Component {
     render() {
         return (
             <div className="homepage-video">
-                {/* <img className="homepage-video" src="assets/splash-temp.jpg"/> */}
                 <video 
                     autoPlay loop
                     // src="assets/homepagevideo_short_version2.98c5bfce.mp4"
+                    poster="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/hero-img.5353d1d3.jpg"
                     src={window.splashVideo}
                     type='video/mp4'
                 >
