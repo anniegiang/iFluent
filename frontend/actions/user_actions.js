c1b853af25cf34fbccfd333eb1dd8645bdd34c60
Annie Giang <annieg4123@gmail.com>
2019-09-14 19:48:56 -0700

    added more actions to users reducer

---
+++ frontend/actions/user_actions.js
@@ -0,0 +1,25 @@
+import * as APIUtil from '../util/users_api_util';
+export const RECEIVE_ALL_USERS = "RECEIVE_ALL_USERS";
+export const RECEIVE_USER = "RECEIVE_USER";
+
+const receiveAllUsers = users => ({
+    type: RECEIVE_ALL_USERS,
+    users
+});
+
+const receiveUser = user => ({
+    type: RECEIVE_USER,
+    user
+});
+
+// thunk 
+
+export const fetchAllUsers = () => dispatch => {
+    return APIUtil.fetchAllUsers()
+        .then(users => dispatch(receiveAllUsers(users)));
+};
+    
+export const fetchUser = id => dispatch => {
+    return APIUtil.fetchUser(id)
+        .then(user => dispatch(receiveUser(user)));
+}
--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,10 +1,15 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_ALL_USERS, RECEIVE_USER } from '../actions/user_actions';
 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
       return Object.assign({}, { [action.currentUser.id]: action.currentUser });
+    case RECEIVE_ALL_USERS:
+      return Object.assign({}, state, action.users);
+    case RECEIVE_USER:
+      return action.user;
     default:
       return state;
   }
