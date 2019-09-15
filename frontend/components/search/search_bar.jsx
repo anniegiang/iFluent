3192d38b0e96b6960c06060b14b4cec22210062b
Annie Giang <annieg4123@gmail.com>
2019-09-15 16:38:56 -0700

    finished last touches of search container styling

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -8,7 +8,7 @@ class SearchBar extends React.Component {
             query: "",
         }
         this.handleDropDown = this.handleDropDown.bind(this);    
-        this.handleInputChange = this.handleInputChange.bind(this); 
+        // this.handleInputChange = this.handleInputChange.bind(this); 
     }
 
     componentDidMount() {
@@ -22,28 +22,28 @@ class SearchBar extends React.Component {
         )
     }
 
-    handleInputChange(e) {
-        this.setState({ query: e.target.value }, () => {
-            console.log(this.state.query)
-        });
-    }
+    // handleInputChange(e) {
+    //     this.setState({ query: e.target.value }, () => {
+    //         console.log(this.state.query)
+    //     });
+    // }
 
     render() {
         return (
             <div className="homepage-menu">
-                
                 <form className="homepage-menu-choice">
                     <input 
                         className="homepage-menu-select" 
                         placeholder="Choose a language"
                         type="text"  
-                        onChange={this.handleInputChange} 
+                        // onChange={this.handleInputChange} 
                         onClick={this.handleDropDown} 
                         />
-                    <button className="homepage-search-icon">
-                        <span><img src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/icon-search.5d60d25a.svg"/></span>
-                    </button>
+                    <span className="homepage-search-icon">
+                        <img src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/icon-search.5d60d25a.svg" alt="search"/>
+                    </span>
                 </form>
+                
             </div>
         )
     }
