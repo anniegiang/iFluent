49ec60097c15fa0579ca418ea35ab253ea5e51ea
Annie Giang <annieg4123@gmail.com>
2019-09-16 22:15:42 -0700

    added right card

--- frontend/components/search/teacher_right_card.jsx
+++ frontend/components/search/teacher_right_card.jsx
@@ -3,7 +3,45 @@ import React from 'react';
 class TeacherRightCard extends React.Component {
     render() {
         return (
-            <div>
+            <div className="teacher-card-right">
+                <div className="teacher-card-tab-head">
+                    <div className="teacher-card-tabs">
+                        <div className="teacher-card-tab">
+                            <p><span>Video</span></p>
+                        </div>
+                        <div className="teacher-card-tab">
+                            <p><span>Intro</span></p>
+                        </div>
+                        <div className="teacher-card-tab">
+                            <p><span>Calendar</span></p>
+                        </div>
+                    </div>
+                </div>
+                <div className="teacher-card-tab-body">
+                    <div className="teacher-card-video">
+                        <div className="iframe-video">
+                            <div className="video-player">
+                                <img src="https://img.youtube.com/vi/fYwG2vINgxw/0.jpg" alt=""/>
+                                {/* <iframe 
+                                    title="video" 
+                                    width="343" 
+                                    height="193" 
+                                    src="https://www.youtube.com/embed/fYwG2vINgxw?autoplay=1&amp;enablejsapi=1&amp;modestbranding=1&amp;rel=0&amp;showinfo=0" 
+                                    frameBorder="0" 
+                                    allow="autoplay" 
+                                    allowFullScreen="" 
+                                    style={{display: "block"}}
+                                ></iframe> */}
+
+                                <div className="video-poster-overlay">
+                                    <i className="video-play-icon"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         )
     }
