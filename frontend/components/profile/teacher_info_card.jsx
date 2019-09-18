5929344919a1f18e189bcae842b93f64ef0820b7
Annie Giang <annieg4123@gmail.com>
2019-09-18 11:55:48 -0700

    added fav and share btn

--- frontend/components/profile/teacher_info_card.jsx
+++ frontend/components/profile/teacher_info_card.jsx
@@ -41,9 +41,90 @@ class TeacherInfoCard extends React.Component {
                                 </div>
                                 <div className="redbar"></div>
                             </div>
+                            <div className="teacherCard-right">
+                                <div className="teacherCard-right-icon">
+                                    <span className="Favourite">
+                                        <img width="24" height="24" src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/favourite.63cfc0be.svg" alt="fav"/>
+                                    </span>
+                                    <div className="share-teacher-wrapper">
+                                        <div class="TagMenu TagMenu-desktop TagMenu-rerotate90">
+                                            <div class="chat-send-attachment-btn">
+                                                <img src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/shareBtn.5fa47672.svg" alt="shares" width="24" height="24"/>
+                                            </div>
+                                        <div class="TagMenu-menu TagMenu-menu-bottom-right TagMenu-hidden">
+                                            <div class="TagMenu-menu-option" data-value="1">
+                                                Share this teacher
+                                            </div>
+                                            <div class="TagMenu-menu-option" data-value="2">
+                                                Report this teacher
+                                            </div>
+                                            <div class="TagMenu-menu-option" data-value="3">
+                                                Block this teacher
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="teacherCard-right-body">
 
+                                </div>
+                            </div>
                         </div>
-
+                        <div className="teacherCard-part2">
+                            <div className="teacherCard-teaches">
+                                <div className="teacherCard-teaches-title">
+                                    <span>Teaches</span>
+                                </div>
+                                <div className="teacherCard-teaches-languages">
+                                    <div>
+                                        <span className="language">
+                                            <span>Placeholder</span>
+                                        </span>
+                                        <span className="tooltip-container-box" gap="5">
+                                            <span className="tooltip-container" placement="bottom">
+                                                <span className="tooltip-reference">
+                                                    <div>
+                                                        <span className="level level-color-2 level-color-3"></span>
+                                                        <span className="level level-color-2 level-color-3"></span>
+                                                        <span className="level level-color-2 level-color-3"></span>
+                                                        <span className="level level-color-2 level-color-3"></span>
+                                                        <span className="level level-color-2 level-color-3"></span>
+                                                    </div>
+                                                </span>
+                                            </span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="teacherCard-teaches">
+                                <div className="teacherCard-teaches-title">
+                                    <span>Also Speaks</span>
+                                </div>
+                                <div className="teacherCard-teaches-languages">
+                                    <div>
+                                        <span className="language">
+                                            <span>Placeholder</span>
+                                        </span>
+                                        <span className="tooltip-container-box" gap="5">
+                                            <span className="tooltip-container" placement="bottom">
+                                                <span className="tooltip-reference">
+                                                    <div>
+                                                        <span className="level level-color-2 level-color-3"></span>
+                                                        <span className="level level-color-2 level-color-3"></span>
+                                                        <span className="level level-color-2 level-color-3"></span>
+                                                        <span className="level level-color-2 level-color-3"></span>
+                                                        <span className="level level-color-2 level-color-3"></span>
+                                                    </div>
+                                                </span>
+                                            </span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="button" className="teacherCard-switch-profile-btn btn btn-standard btn-ghost-default">
+                            <span>Switch to User Profile</span>
+                        </button>
                     </div>
                 </div>
             </div>
