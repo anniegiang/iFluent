f9bbc661fb8f21a45774cde4fa5c5ae9a47f4232
Annie Giang <annieg4123@gmail.com>
2019-09-29 16:55:28 -0700

    add homepage hero background gradient

--- frontend/components/splash/homepage/homepage_hero/homepage_hero.jsx
+++ frontend/components/splash/homepage/homepage_hero/homepage_hero.jsx
@@ -13,6 +13,8 @@ class HomePageHero extends React.Component {
             src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/homepagevideo_short_version2.98c5bfce.mp4"
           ></video>
         </div>
+
+        <div className="homepage-hero_background"></div>
       </div>
     );
   }
