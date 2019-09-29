c8c36a9d78e57fed3fa5382066c7f8dd173fdd01
Annie Giang <annieg4123@gmail.com>
2019-09-28 21:51:05 -0700

    add scrolling logic for splash nav

--- frontend/components/navbar/splash_navbar.jsx
+++ frontend/components/navbar/splash_navbar.jsx
@@ -2,6 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class SplashNavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll() {
+    if (this.props.location.pathname.includes("/teachers")) {
+      return;
+    }
+
+    let nav = document.querySelector(".splash-nav-container");
+    if (window.scrollY > 70) {
+      nav.classList.add("scrolled");
+    } else {
+      nav.classList.remove("scrolled");
+    }
+  }
   render() {
     return (
       <nav className="splash-nav-container">
