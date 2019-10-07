04cc30ad87e8f3b61db8121626f6c2ece4b74c62
Annie Giang <annieg4123@gmail.com>
2019-10-07 11:25:46 -0700

    Style search results container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -9,17 +9,21 @@ import Landing from "./landing/landing";
 import SearchResultsContainer from "./search_results/search_results_container";
 
 const App = () => (
-  <div className="app">
-    <Modal />
-    <Switch>
-      <Route
-        exact
-        path="/teachers/:languageName"
-        component={SearchResultsContainer}
-      />
-      <ProtectedRoute exact path="/dashboard" component={Landing} />
-      <AuthRoute exact path="/" component={Splash} />
-    </Switch>
+  <div className="app app-paddingTop-0">
+    <div className="flex-container">
+      <div style={{ position: "static", zIndex: "inherit" }}>
+        <Modal />
+        <Switch>
+          <Route
+            exact
+            path="/teachers/:languageName"
+            component={SearchResultsContainer}
+          />
+          <ProtectedRoute exact path="/dashboard" component={Landing} />
+          <AuthRoute exact path="/" component={Splash} />
+        </Switch>
+      </div>
+    </div>
   </div>
 );
 
--- frontend/components/search_results/search_teacher_card.jsx
+++ frontend/components/search_results/search_teacher_card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const TeacherCard = props => {
   return (
     <div className="search-teacher-card_container">
-      <h1>{props.teacher.name}</h1>
+      
     </div>
   );
 };
