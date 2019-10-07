8c37fbe8444123801712ccbc5d58f38a6106ec67
Annie Giang <annieg4123@gmail.com>
2019-10-07 12:12:09 -0700

    display teacher profile picture

--- frontend/components/search_results/search_teacher_card_left.jsx
+++ frontend/components/search_results/search_teacher_card_left.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 const TeacherCardLeft = props => {
+  const {
+    id,
+    teacher_id,
+    country,
+    about_me,
+    title,
+    video_url,
+    hourly_rate,
+    trial_rate,
+    name,
+    profilePicture
+  } = props.teacher;
+
   return (
     <div className="search-teacher-card_left">
-
+      <div className="search-teacher-card_left__top">
+        <div className="search-teacher-card_left__top__avatar">
+          <div class="avatar avatar-big avatar-shadow avatar-placeholder">
+            <img src={profilePicture} alt="Avatar" />
+          </div>
+        </div>
+      </div>
+      <div className="search-teacher-card_left__bottom"></div>
     </div>
-  )
+  );
 };
 
 export default TeacherCardLeft;
