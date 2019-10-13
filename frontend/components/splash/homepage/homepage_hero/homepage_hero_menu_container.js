9c0be0fa6c267ac036ea9b53a3f2f72f34211937
Annie Giang <annieg4123@gmail.com>
2019-10-13 12:24:37 -0700

    fix import path for fetchLanguages

--- frontend/components/splash/homepage/homepage_hero/homepage_hero_menu_container.js
+++ frontend/components/splash/homepage/homepage_hero/homepage_hero_menu_container.js
@@ -1,7 +1,8 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import HomePageHeroMenu from "./homepage_hero_menu";
-import { fetchAllLanguages } from "/Users/Annie/Desktop/app-academy/ifluent/frontend/actions/language_actions.js";
+import { fetchAllLanguages } from "../../../../actions/language_actions.js";
+// import { fetchAllLanguages } from "/Users/Annie/Desktop/app-academy/ifluent/frontend/actions/language_actions.js";
 
 const msp = state => {
   return {
