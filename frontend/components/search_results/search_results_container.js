879e4c900136cb0aa5519cae0283d1a0cf62aa72
Annie Giang <annieg4123@gmail.com>
2019-10-13 12:25:54 -0700

    fix import path for search results

--- frontend/components/search_results/search_results_container.js
+++ frontend/components/search_results/search_results_container.js
@@ -1,7 +1,8 @@
 import SearchResults from "./search_results";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { fetchAllTeachersByLanguage } from "/Users/Annie/Desktop/app-academy/ifluent/frontend/actions/teacher_actions.js";
+import { fetchAllTeachersByLanguage } from "../../actions/teacher_actions.js";
+// import { fetchAllTeachersByLanguage } from "/Users/Annie/Desktop/app-academy/ifluent/frontend/actions/teacher_actions.js";
 
 const msp = state => {
   return {
