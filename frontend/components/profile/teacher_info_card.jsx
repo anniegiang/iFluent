3b2cfb8f98af7ca54684cc487b39c7506e9dd37b
Annie Giang <annieg4123@gmail.com>
2019-10-19 21:00:28 -0700

    remove share and favorite button in teacher profile

--- frontend/components/profile/teacher_info_card.jsx
+++ frontend/components/profile/teacher_info_card.jsx
@@ -40,10 +40,7 @@ const TeacherInfoCard = props => {
         <div className="teacherCard-body">
           <div className="teacherCard-part1">
             <div className="teacherCard-middle">
-              <div className="teacherCard-name">
-                {name}
-                {/* <img src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/am.a5752aba.svg" alt="AM" width="30px" height="16px" className="flag-icon"/> */}
-              </div>
+              <div className="teacherCard-name">{name}</div>
               <div className="teacherCard-personalInfo">
                 <div>
                   <span>{props.teacher.title}</span>
@@ -60,39 +57,6 @@ const TeacherInfoCard = props => {
               <div className="redbar"></div>
             </div>
             <div className="teacherCard-right">
-              <div className="teacherCard-right-icon">
-                <span className="Favourite">
-                  <img
-                    width="24"
-                    height="24"
-                    src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/favourite.63cfc0be.svg"
-                    alt="fav"
-                  />
-                </span>
-                <div className="share-teacher-wrapper">
-                  <div className="TagMenu TagMenu-desktop TagMenu-rerotate90">
-                    <div className="chat-send-attachment-btn">
-                      <img
-                        src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/shareBtn.5fa47672.svg"
-                        alt="shares"
-                        width="24"
-                        height="24"
-                      />
-                    </div>
-                    <div className="TagMenu-menu TagMenu-menu-bottom-right TagMenu-hidden">
-                      <div className="TagMenu-menu-option" data-value="1">
-                        Share this teacher
-                      </div>
-                      <div className="TagMenu-menu-option" data-value="2">
-                        Report this teacher
-                      </div>
-                      <div className="TagMenu-menu-option" data-value="3">
-                        Block this teacher
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
               <div className="teacherCard-right-body">
                 <div className="teacherCard-stars">
                   <div className="stars-box">
@@ -165,9 +129,6 @@ const TeacherInfoCard = props => {
               </div>
             </div>
           </div>
-          {/* <button type="button" className="teacherCard-switch-profile-btn btn btn-standard btn-ghost-default">
-                            <span>Switch to User Profile</span>
-                        </button> */}
         </div>
       </div>
       <div className="aboutMe">
@@ -175,17 +136,7 @@ const TeacherInfoCard = props => {
           <h2 className="cardTitle">
             <span>About Me</span>
           </h2>
-          <div className="aboutMe-introduction">
-            {aboutMe}
-            {/* <div className="TextOverflow" style={{maxHeight: "63px", lineHeight: "21px"}}>
-                                    <span className="TextOverflow-readMore">
-                                        <span className="TextOverflow-readMore-ellipsis">&nbsp;...</span>
-                                        <span className="TextOverflow-readMore-text">
-                                            <span>Read more</span>
-                                        </span>
-                                    </span>
-                                </div> */}
-          </div>
+          <div className="aboutMe-introduction">{aboutMe}</div>
         </div>
       </div>
     </div>
