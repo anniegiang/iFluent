32d3742c26f7ef0ece128134890163824537ac89
Annie Giang <annieg4123@gmail.com>
2019-10-23 10:00:35 -0700

    attempt to fix webpack build issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/ifluent.jsx",
+  entry: './frontend/ifluent.jsx',
   output: {
-    path: path.resolve(__dirname, "app", "assets", "javascripts"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
   },
   resolve: {
-    extensions: [".js", ".jsx", "*"]
+    extensions: ['.js', '.jsx', '*']
   },
   module: {
     rules: [
@@ -16,21 +16,21 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           query: {
-            presets: ["@babel/env", "@babel/react"],
-            plugins: ["@babel/plugin-proposal-class-properties"]
+            presets: ['@babel/env', '@babel/react'],
+            plugins: ['@babel/plugin-proposal-class-properties']
           }
         }
       },
       {
         test: /\.css$/i,
-        loader: "css-loader",
+        loader: 'css-loader',
         options: {
           modules: true
         }
       }
     ]
   },
-  devtool: "source-map"
-};
+  devtool: 'source-map'
+}
