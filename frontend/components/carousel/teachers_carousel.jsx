c08e8eecca93ab18c4c13706c9ed3533eee2b1be
Annie Giang <annieg4123@gmail.com>
2019-10-26 23:17:36 -0700

    Display 6 slides per dot and style title

--- frontend/components/carousel/teachers_carousel.jsx
+++ frontend/components/carousel/teachers_carousel.jsx
@@ -1,20 +1,46 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
+
 class TeachersCarousel extends Component {
   render() {
     const settings = {
       className: "center",
-      infinite: true,
-      centerPadding: "60px",
-      slidesToShow: 3,
       speed: 500,
       rows: 2,
-      slidesPerRow: 2
+      slidesPerRow: 3,
+      dots: true,
+      infinite: true,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />
     };
     return (
-      <div>
-        <h2>Multiple Rows</h2>
+      <div className="homepage-teachers-slider">
+        <h1>
+          10,000+ teachers.
+          <span class="homepage-anylanguage"> Any language.</span>
+        </h1>
         <Slider {...settings}>
           <div>
             <h3>1</h3>
@@ -63,6 +89,12 @@ class TeachersCarousel extends Component {
           </div>
           <div>
             <h3>16</h3>
+          </div>
+          <div>
+            <h3>17</h3>
+          </div>
+          <div>
+            <h3>18</h3>
           </div>
         </Slider>
       </div>
