4674d74a4e22f6a05e93ed97d3435d742174fdf4
Annie Giang <annieg4123@gmail.com>
2019-11-10 14:36:17 -0800

    update asset links to aws

--- frontend/components/splash/homepage/homepage_why_italki/why_italki.jsx
+++ frontend/components/splash/homepage/homepage_why_italki/why_italki.jsx
@@ -8,19 +8,19 @@ const WhyItalki = props => {
         className="homepage-why-tialki-learing"
         h1Text="Customized Learning"
         pText="Choose from over 10,000 teachers for 1-on-1 lessons based on your goals and interests."
-        imgUrl="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/illustrations-01.14a9dc27.svg"
+        imgUrl="https://ifluent-dev.s3-us-west-1.amazonaws.com/illustrations-01-14a9dc27.svg"
       />
       <WhyItalkiItem
         className="homepage-why-tialki-pay"
         h1Text="Pay Per Lesson"
         pText="On iFluent you only pay per lesson and at the price that meets your budget."
-        imgUrl="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/illustrations-02.e3592c22.svg"
+        imgUrl="https://ifluent-dev.s3-us-west-1.amazonaws.com/illustrations-02-e3592c22.svg"
       />
       <WhyItalkiItem
         className="homepage-why-tialki-any"
         h1Text="Anytime, Anywhere"
         pText="Take online lessons at the time and place that suits you"
-        imgUrl="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/illustrations-03.a64719fe.svg"
+        imgUrl="https://ifluent-dev.s3-us-west-1.amazonaws.com/illustrations-03-a64719fe.svg"
       />
     </div>
   );
