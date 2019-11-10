857ae34d677af0594e9ee06bfeed4390fffee774
Annie Giang <annieg4123@gmail.com>
2019-11-10 15:48:26 -0800

    change flag links to aws and formart size

--- frontend/components/carousel/teachers_carousel.jsx
+++ frontend/components/carousel/teachers_carousel.jsx
@@ -29,29 +29,34 @@ class TeachersCarousel extends Component {
       {
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/english-UK-0b1dc079.jpg",
-        flag: "https://www.italki.com/static/media/usa.4b11fcae.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/uk.png",
         language: "English"
       },
       {
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/spanish-32d567a0.jpg",
-        flag: "https://www.italki.com/static/media/usa.4b11fcae.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/spain.png",
         language: "Spanish"
       },
       {
-        flag: "https://www.italki.com/static/media/france.4b6deacc.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/france.svg",
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/french-87f7b7cf.jpg",
         language: "French"
       },
       {
-        flag: "https://www.italki.com/static/media/japan.60b87b49.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/japan.svg",
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/japanese-99636b79.jpg",
         language: "Japanese"
       },
       {
-        flag: "https://www.italki.com/static/media/china.1156edea.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/china.svg",
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/chinese-ba590708.jpg",
         language: "Chinese"
@@ -59,17 +64,20 @@ class TeachersCarousel extends Component {
       {
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/german-8ce1eca6.jpg",
-        flag: "https://www.italki.com/static/media/germany.ab4f232a.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/germany.svg",
         language: "German"
       },
       {
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/italian-c4abb062.jpg",
-        flag: "https://www.italki.com/static/media/italy.62b3f764.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/italy.svg",
         language: "Italian"
       },
       {
-        flag: "https://www.italki.com/static/media/russia.ee120f11.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/russia.svg",
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/russian-c6c8acfd.jpg",
         language: "Russian"
@@ -77,11 +85,13 @@ class TeachersCarousel extends Component {
       {
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/portuguese-e159c420.jpg",
-        flag: "https://www.italki.com/static/media/usa.4b11fcae.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/portuguese.png",
         language: "Portuguese"
       },
       {
-        flag: "https://www.italki.com/static/media/korea.75ac9327.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/korea.svg",
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/korean-a0d01156.jpg",
         language: "Korean"
@@ -89,29 +99,34 @@ class TeachersCarousel extends Component {
       {
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/arabic-5d7f2efc.jpg",
-        flag: "https://www.italki.com/static/media/usa.4b11fcae.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/arabic.png",
         language: "Arabic"
       },
       {
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/dutch-5e6ef5bd.jpg",
-        flag: "https://www.italki.com/static/media/nederland.8c94355c.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/nederland.svg",
         language: "Dutch"
       },
       {
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/turkish-3433d42b.jpg",
-        flag: "https://www.italki.com/static/media/usa.4b11fcae.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/turkish.png",
         language: "Turkish"
       },
       {
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/hebrew-faaf0b13.jpg",
-        flag: "https://www.italki.com/static/media/israel.ab315ab1.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/israel.svg",
         language: "Hebrew"
       },
       {
-        flag: "https://www.italki.com/static/media/poland.ba65fa93.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/poland.svg",
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/polish-faad616a.jpg",
         language: "Polish"
@@ -119,13 +134,15 @@ class TeachersCarousel extends Component {
       {
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/hindi-5c1a2b79.jpg",
-        flag: "https://www.italki.com/static/media/usa.4b11fcae.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/hindi.png",
         language: "Hindi"
       },
       {
         img:
           "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/serbian-cae18b82.jpg",
-        flag: "https://www.italki.com/static/media/usa.4b11fcae.svg",
+        flag:
+          "https://ifluent-dev.s3-us-west-1.amazonaws.com/teachers-slider/countries/seberia.png",
         language: "Serbian"
       },
       {
