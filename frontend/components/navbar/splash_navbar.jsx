ad707b70ea3b6aa65ae256008514ae6cacaf86ef
Annie Giang <annieg4123@gmail.com>
2019-12-02 15:30:00 -0800

    Remove social links in nav

--- frontend/components/navbar/splash_navbar.jsx
+++ frontend/components/navbar/splash_navbar.jsx
@@ -31,30 +31,9 @@ class SplashNavBar extends React.Component {
     return (
       <nav className="splash-nav-container">
         <a href="/" className="splash-logo">
-          <img
-            src={window.logo}
-            // src="https://d1m3ds7i7t710d.cloudfront.net/orion/static/media/logo_white.72e72948.svg"
-            alt="splash-logo"
-          />
+          <img src={window.logo} alt="splash-logo" />
         </a>
         <div className="splash-nav-links">
-          <a
-            className="social"
-            href="https://github.com/anniegiang"
-            target="_blank"
-          >
-            Github
-          </a>
-          <a
-            href="https://www.linkedin.com/in/annie-giang-113bb4101/"
-            target="_blank"
-            className="social"
-          >
-            Linkedin
-          </a>
-          <a href="https://anniegiang.me/" target="_blank" className="social">
-            Portfolio
-          </a>
           <button onClick={this.props.openModalLogin}>Log In</button>
           <button onClick={this.props.openModalSignup}>Sign Up</button>
         </div>
